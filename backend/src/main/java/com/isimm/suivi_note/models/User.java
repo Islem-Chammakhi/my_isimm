937b3034be465ab77e1f4463c5603270fb6467d7
Mohamed Yassine Kharrat <med.yassine.kharrat@gmail.com>
2026-05-09 23:24:14 +0100

    refactor: Turned role into DiscriminatorColumn, and changed inheritance type to SINGLE Changed inheritance because number of columns in t_users + sub classes is small. => JOIN operation is more expensive than stuffing them all in one table

--- backend/src/main/java/com/isimm/suivi_note/models/User.java
+++ backend/src/main/java/com/isimm/suivi_note/models/User.java
@@ -4,29 +4,19 @@ import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.isimm.suivi_note.enums.Role;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.experimental.SuperBuilder;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name="user_type")
+@DiscriminatorColumn(name="role", discriminatorType = DiscriminatorType.STRING)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // Because number of columns in t_users + sub classes is small. => Better than JOIN
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -37,30 +27,38 @@ public class User  implements UserDetails {
     
     @Id
     private String cin ;
+
     @NotBlank(message = "firstName is missing")
     private String firstName; 
+
     @NotBlank(message = "lastName is missing")
     private String lastName; 
+
     @NotBlank(message = "email is missing")
     @Email(message = " invalid email")
     @Column(unique = true, nullable = false)
     private String email;
+
     @NotBlank(message = "password is missing")
     private String password;
 
-    @Column
     private String otp;
 
-    @Column
     private LocalDateTime otpExpiry;
-
+/*  DiscriminatorColumn makes this obsolete
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private Role role ;
+    private Role role;*/
+
+    @Transient
+    public String getRole(){
+        return getClass().getAnnotation(DiscriminatorValue.class).value();
+    }
 
     @Override
+    //TODO: Test this. Make sure no kaboom happens
     public Collection<? extends GrantedAuthority> getAuthorities() {
-       return List.of(new SimpleGrantedAuthority("ROLE_" + this.role.name()));
+       return List.of(new SimpleGrantedAuthority("ROLE_" + getClass().getAnnotation(DiscriminatorValue.class).value()));
     }
 
     @Override
